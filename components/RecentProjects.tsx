3242fcfa2bb10c170a5f720d09daf5df2147ae69
as10ae <abdussamada11d8@gmail.com>
2025-06-10 16:27:04 +0400

    Recent Project And Navbar completed

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,16 +1,83 @@
+"use client";
+
+import { FaLocationArrow } from "react-icons/fa6";
 import { projects } from "@/data";
-import React from "react";
+import { PinContainer } from "./ui/3d-pin";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
       <h1 className="heading">
-        A Small Selection of{" "}
-        <span className="text-purple">Recent Projects</span>
+        A small selection of{" "}
+        <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
-        {projects.map(({ id, title, des, img, iconLists, link }) => (
-          <div key={id}>{title}</div>
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
+        {projects.map((item) => (
+          <a
+            href={item.link}
+            key={item.id}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="sm:h-[41rem] lg:min-h-[32.5rem] h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw] no-underline"
+          >
+            <PinContainer title={item.title}>
+              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10 pointer-events-none">
+                <div
+                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                  style={{ backgroundColor: "#13162D" }}
+                >
+                  <img
+                    src="/bg.png"
+                    alt="bgimg"
+                    className="pointer-events-none"
+                  />
+                </div>
+                <img
+                  src={item.img}
+                  alt="cover"
+                  className="z-10 absolute bottom-0 pointer-events-none"
+                />
+              </div>
+
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {item.title}
+              </h1>
+
+              <p
+                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                style={{ color: "#BEC1DD", margin: "1vh 0" }}
+              >
+                {item.des}
+              </p>
+
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center">
+                  {item.iconLists.map((icon, index) => (
+                    <div
+                      key={index}
+                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{
+                        transform: `translateX(-${5 * index + 2}px)`,
+                      }}
+                    >
+                      <img
+                        src={icon}
+                        alt="icon"
+                        className="p-2 pointer-events-none"
+                      />
+                    </div>
+                  ))}
+                </div>
+
+                <div className="flex justify-center items-center">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    Check Live Site
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
+                </div>
+              </div>
+            </PinContainer>
+          </a>
         ))}
       </div>
     </div>
